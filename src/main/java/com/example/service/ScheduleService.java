4ad3c621fd2d53aa2895cc1688219f721200ff09
KWAGDAEYONG <dayong7387@hanmail.net>
2017-06-15 21:40:10 +0900

    queryDsl

--- src/main/java/com/example/service/ScheduleService.java
+++ src/main/java/com/example/service/ScheduleService.java
@@ -19,8 +19,11 @@ public class ScheduleService {
     public Schedule add(Schedule schedule){
         return scheduleRepository.save(schedule);
     }
-    public List<Schedule> prevOrNextSchedule(int month){return  scheduleRepository.findByMonth(month);}
     public List<Schedule> selectByUser(User userId){
         return scheduleRepository.findByUserId(userId);
+    }
+
+    public List<Schedule> selectByMonth(String date){
+        return scheduleRepository.selectByMonth(date);
     }
 }
